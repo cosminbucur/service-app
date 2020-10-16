bf31e85e426fd06475c607622f8ad8070c953c7b
AlexD <asdragomir@gmail.com>
2020-10-16 19:46:17 +0300

    added console and jpa properties

--- src/main/java/com/fm/Application.java
+++ src/main/java/com/fm/Application.java
@@ -6,9 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Application {
 
-    // TODO: add in memory database support
-    // add H2 database dependency
-    // configure db in application.yml
     @RequestMapping("/")
     public static void main(String[] args) {
 
