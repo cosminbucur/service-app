f6badd3585556d51b56a4981b7fa33950ed1f4d6
cosmin bucur <cosmin.bucur@kambi.com>
2020-10-19 13:23:34 +0300

    code review

--- src/main/java/com/fm/Application.java
+++ src/main/java/com/fm/Application.java
@@ -1,12 +1,8 @@
 package com.fm;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
+// TODO: convert to spring boot application
 public class Application {
 
-    @RequestMapping("/")
     public static void main(String[] args) {
 
     }
