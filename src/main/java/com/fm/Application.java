3a4951352d3c1277fd7f7d1e7aff0b307d6cb1c5
AlexD <asdragomir@gmail.com>
2020-10-20 16:36:03 +0300

    convert to spring boot application

--- src/main/java/com/fm/Application.java
+++ src/main/java/com/fm/Application.java
@@ -1,9 +1,14 @@
 package com.fm;
 
-// TODO: convert to spring boot application
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
 
     }
 }
