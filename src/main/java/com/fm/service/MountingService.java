8e36f6db978718e1b069bf5f1b8839efa71f4bdb
AlexD <asdragomir@gmail.com>
2020-10-23 23:56:53 +0300

    removed Vehicle dto and associated code but saved builder example

--- src/main/java/com/fm/service/MountingService.java
+++ src/main/java/com/fm/service/MountingService.java
@@ -2,7 +2,6 @@ package com.fm.service;
 
 import com.fm.dto.CustomerVisitDetails;
 import com.fm.dto.TyreDetail;
-import com.fm.dto.Vehicle;
 import com.fm.model.Mechanic;
 
 import java.time.LocalDate;
@@ -13,15 +12,11 @@ public class MountingService {
 
     private List<TyreDetail> newTyreDetails = new ArrayList<>();
 
-    public void replaceTyres(CustomerVisitDetails customerVisitDetails, long mechanicId, String licensePlate, Vehicle vehicleWithOldTyres, Vehicle vehicleWithNewTyres) {
+    public void replaceTyres(CustomerVisitDetails customerVisitDetails, long mechanicId, String licensePlate) {
 
         Mechanic.mechanic().setId(mechanicId);
 
         customerVisitDetails.setVisitDate(LocalDate.now());
         customerVisitDetails.getCustomerDetails().setLicensePlate(licensePlate);
-
-        vehicleWithOldTyres.licensePlate = customerVisitDetails.getCustomerDetails().getLicensePlate();
-        vehicleWithNewTyres.licensePlate = vehicleWithOldTyres.licensePlate;
-        vehicleWithNewTyres.mountTyres(newTyreDetails);
     }
 }
--- src/test/java/com/fm/acceptance/WhenNewCustomerTest.java
+++ src/test/java/com/fm/acceptance/WhenNewCustomerTest.java
@@ -2,7 +2,6 @@ package com.fm.acceptance;
 
 import com.fm.dto.CustomerVisitDetails;
 import com.fm.dto.TyreDetail;
-import com.fm.dto.Vehicle;
 import com.fm.model.TyreType;
 import com.fm.model.WearLevel;
 import com.fm.service.MountingService;
@@ -10,8 +9,6 @@ import com.fm.util.TestUtils;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 class WhenNewCustomerTest {
 
@@ -28,24 +25,7 @@ class WhenNewCustomerTest {
 
         MountingService mountingService = new MountingService();
 
-        Vehicle vehicleWithOldTyres = new Vehicle.Builder()
-            .withFrontLeft(TestUtils.createTyre(TyreType.SUMMER, WearLevel.GOOD))
-            .withFrontRight(TestUtils.createTyre(TyreType.SUMMER, WearLevel.GOOD))
-            .withRearLeft(TestUtils.createTyre(TyreType.SUMMER, WearLevel.GOOD))
-            .withRearRight(TestUtils.createTyre(TyreType.SUMMER, WearLevel.GOOD))
-            .build();
-
         TyreDetail newFrontLeft = TestUtils.createTyre(TyreType.WINTER, WearLevel.GOOD);
 
-        Vehicle vehicleWithNewTyres = new Vehicle.Builder()
-            .withFrontLeft(newFrontLeft)
-            .withFrontRight(TestUtils.createTyre(TyreType.WINTER, WearLevel.GOOD))
-            .withRearLeft(TestUtils.createTyre(TyreType.WINTER, WearLevel.GOOD))
-            .withRearRight(TestUtils.createTyre(TyreType.WINTER, WearLevel.GOOD))
-            .build();
-
-        mountingService.replaceTyres(customerVisitDetails, mechanicId, licensePlate, vehicleWithOldTyres, vehicleWithNewTyres);
-
-        assertThat(vehicleWithNewTyres.frontLeft).isEqualTo(newFrontLeft);
     }
 }
